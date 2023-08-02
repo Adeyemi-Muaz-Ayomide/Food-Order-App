e5e513f93825431b94fa46888a8a6e09159a9951
iAmMuadh <adeyemimuaz1@gmail.com>
2023-08-02 01:36:03 +0100

    Button component added

---
+++ src/components/Meals/MealsAmount.js
@@ -0,0 +1,11 @@
+
+const MealsAmount = () => {
+  return (
+    <div>
+        <h3>Amount</h3>
+        <input type="number" min='0' max='40' step='1' />
+    </div>
+  )
+}
+
+export default MealsAmount
